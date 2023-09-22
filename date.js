fc3e19ca50e8558811533cb16bcf42be615d9f99
hari <harichandmanoj@gmail.com>
2023-09-22 18:03:54 +0530

    feat: date

---
+++ date.js
@@ -0,0 +1,24 @@
+
+const d= new Date();
+let day=d.getDay()
+
+if(day==0){
+    document.write("Sunday")
+} else if(day==1){
+    document.write("monday")
+}
+else if(day==2){
+    document.write("Tuesday")
+}
+else if(day==3){
+    document.write("Wednesday")
+}
+else if(day==4){
+    document.write("Thursday")
+}
+else if(day==5){
+    document.write("Friday")
+}
+else if(day==6){
+    document.write("Saturday")
+}
